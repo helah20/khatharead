1c383ff3e9dc82c7d0e8c0a1ea6e82413cd91e83
hela <48882360+helah20@users.noreply.github.com>
2022-01-28 21:08:38 +0300

    Add files via upload

---
+++ history/js/index.js
@@ -0,0 +1,13 @@
+ var firebaseConfig =
+ {
+    apiKey: "AIzaSyB7GQ3cXZb4cTLU2nXwRZMv231v8aQrQXw",
+    authDomain: "storyteller-2f26a.firebaseapp.com",
+    databaseURL: "https://storyteller-2f26a.firebaseio.com",
+    projectId: "storyteller-2f26a",
+    storageBucket: "storyteller-2f26a.appspot.com",
+    messagingSenderId: "98742849980",
+    appId: "1:98742849980:web:0fdc4c91f86a8461659bb9",
+    measurementId: "G-L3SEF2VKQQ"
+  };
+   // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
